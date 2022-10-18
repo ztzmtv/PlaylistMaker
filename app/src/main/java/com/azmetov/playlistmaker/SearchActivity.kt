639997acc8f735c8471337cd35016885acb40ee5
Azat Azmetov <azmetoff@gmail.com>
2022-10-18 15:19:41 +0300

    переделаны ресурсы. добавлен экран поиска

--- app/src/main/java/com/azmetov/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/azmetov/playlistmaker/SearchActivity.kt
@@ -2,14 +2,34 @@ package com.azmetov.playlistmaker
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchEditText = findViewById<EditText>(R.id.et_search)
+        searchEditText.requestFocus()
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("TAG", "$charSequence")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
     }
 
     companion object {
