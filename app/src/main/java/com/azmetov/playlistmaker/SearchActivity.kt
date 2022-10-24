811922158014c2645978ec41a2a97bcb24670bae
Azat Azmetov <azmetoff@gmail.com>
2022-10-24 17:28:57 +0300

    клавиатура теперь убирается. Более простого способа не нашёл

--- app/src/main/java/com/azmetov/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/azmetov/playlistmaker/SearchActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+
 
 class SearchActivity : AppCompatActivity() {
 
@@ -20,10 +23,10 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         searchEditText = findViewById(R.id.et_search)
+        val searchTextInputLayout = findViewById<TextInputLayout>(R.id.til_search)
+
         searchEditText.requestFocus()
-        val inputMethodManager =
-            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-        inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        hideKeyboard(searchEditText)
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
@@ -33,6 +36,18 @@ class SearchActivity : AppCompatActivity() {
 
             override fun afterTextChanged(p0: Editable?) {}
         })
+
+        searchTextInputLayout.setEndIconOnClickListener {
+            searchEditText.text.clear()
+            hideKeyboard(searchEditText)
+        }
+
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -50,6 +65,5 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         private const val EDIT_TEXT_KEY = "EDIT_TEXT_KEY"
         fun getIntent(context: Context) = Intent(context, SearchActivity::class.java)
-
     }
 }
